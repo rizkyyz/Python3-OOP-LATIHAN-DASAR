60359f6fd1757a10034188ba5bca615a69742d11
Kyky <66506609+rizkyyz@users.noreply.github.com>
2021-02-27 15:01:05 +0700

    Add files via upload

---
+++ template.py
@@ -0,0 +1,10 @@
+#Para Ulama ahli fiqih sepakat atas haramnya harta seorang Muslim dan kafir dzimmi.
+#Harta mereka tidak boleh direbut, diambil, dicuri, dimakan dengan cara yang tidak dibenarkan 
+#syari’at, walaupun sedikit. Allâh Azza wa Jalla berfirman:
+#يَا أَيُّهَا الَّذِينَ آمَنُوا لَا تَأْكُلُوا أَمْوَالَكُمْ بَيْنَكُمْ بِالْبَاطِلِ
+#Rizkyyz Gans
+
+# membuat template print 
+print ("hello rizky")
+print ("hello semua apakabar")
+print ("Semoga Baik Baik Saja")
