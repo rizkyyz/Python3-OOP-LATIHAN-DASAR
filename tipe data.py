ff44b0fc04f7d6b9853b3054f728e2f070e45029
Kyky <66506609+rizkyyz@users.noreply.github.com>
2021-02-27 15:13:38 +0700

    Add files via upload

---
+++ tipe data.py
@@ -0,0 +1,44 @@
+#Para Ulama ahli fiqih sepakat atas haramnya harta seorang Muslim dan kafir dzimmi.
+#Harta mereka tidak boleh direbut, diambil, dicuri, dimakan dengan cara yang tidak dibenarkan 
+#syari’at, walaupun sedikit. Allâh Azza wa Jalla berfirman:
+#يَا أَيُّهَا الَّذِينَ آمَنُوا لَا تَأْكُلُوا أَمْوَالَكُمْ بَيْنَكُمْ بِالْبَاطِلِ
+#Rizkyyz Gans
+
+# membuat template print 
+# a = 10, a adalah variabel dengan nilai 10
+
+# tipe data: Angka satuan yang gak ada
+# koma nya (integer)
+data_integer = 11
+print("data : ", data_integer)
+print("- bertipe : ", type(data_integer))
+
+#  tipe data string 
+data_string = "rizkyyz"
+print("data : ", data_string)
+print("- bertipe : ", type(data_string))
+
+# tipe data float
+data_float = 1.9
+print ("data : ", data_float)
+print ("- bertipe : ", type(data_float))
+
+# tipe data: biner true/false (bollean)
+data_biner = True
+print ("data :",data_biner)
+print ("- bertipe : ", type(data_biner))
+
+## tipe data khusus
+
+# bilangan kompleks
+data_complex = complex(5,6)
+print("data : ", data_complex)
+print("- bertipe : ", type(data_complex))
+
+# tipe data dari bahasa C
+
+from ctypes import c_double, c_char
+
+data_c_double = c_double(10.5)
+print("data : ", data_c_double)
+print("- bertipe : ", type(data_c_double))
