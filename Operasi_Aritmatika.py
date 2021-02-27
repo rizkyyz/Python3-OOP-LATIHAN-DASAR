73bf51dec81204b0f4fc3356a88d13e96c98c7e3
Kyky <66506609+rizkyyz@users.noreply.github.com>
2021-02-27 14:39:38 +0700

    Add files via upload

---
+++ Operasi_Aritmatika.py
@@ -0,0 +1,30 @@
+#Para Ulama ahli fiqih sepakat atas haramnya harta seorang Muslim dan kafir dzimmi.
+#Harta mereka tidak boleh direbut, diambil, dicuri, dimakan dengan cara yang tidak dibenarkan 
+#syari’at, walaupun sedikit. Allâh Azza wa Jalla berfirman:
+#يَا أَيُّهَا الَّذِينَ آمَنُوا لَا تَأْكُلُوا أَمْوَالَكُمْ بَيْنَكُمْ بِالْبَاطِلِ
+#Rizkyyz Gans
+# membuat operasi aritmatika
+print ("====Operasi Aritmatika====")
+
+a = int(input("masukan nilai pertama :"))
+b = int(input("Masukan Nilai Kedua :"))
+# Operasi Penjumlahan
+hasil = a + b
+print(a,'+',b,'=',hasil)
+
+# Operasi Pengurangan 
+hasil = a - b
+print(a,'-',b,'=',hasil)
+
+#Operasi Perkalian
+hasil = a * b
+print(a,'x',b,'=',hasil)
+
+# operasi pembagian
+hasil = a / b
+print(a,'/',b,'=',hasil)
+
+# Operasi Pangkat
+hasil = a ** b
+print(a,'**',b,'=',hasil)
+
