285656f61f4cb58a8b1957a1802b31d82991c460
Kyky <66506609+rizkyyz@users.noreply.github.com>
2021-02-27 14:50:55 +0700

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,34 @@
+#Para Ulama ahli fiqih sepakat atas haramnya harta seorang Muslim dan kafir dzimmi.
+#Harta mereka tidak boleh direbut, diambil, dicuri, dimakan dengan cara yang tidak dibenarkan 
+#syari’at, walaupun sedikit. Allâh Azza wa Jalla berfirman:
+#يَا أَيُّهَا الَّذِينَ آمَنُوا لَا تَأْكُلُوا أَمْوَالَكُمْ بَيْنَكُمْ بِالْبَاطِلِ
+#Rizkyyz Gans
+
+# membuat operasi variable
+
+# menaru / assignment nilai
+a = 10
+b = 15
+panjang = 1000
+
+# pemanggilan nama pertama
+print("Nilai a = ", a)
+print("Nilai x = ", b)
+print("Nilai panjang = ", panjang)
+
+# penamaan pada variable 
+nilai_y = 15 # dengan menggunakan underscore cok
+juta10 = 10000000 # ini boleh
+nilaiP = 17.5 # ini boleh
+
+print("Nilai_y :", nilai_y)
+print("JUTA10 :",juta10)
+
+# pemanggilan kedua
+print("Nilai a = ", a)
+a = 7
+print("Nilai a = ", a)
+
+# assignment indirect
+b = a
+print("Nilai b = ", b)
